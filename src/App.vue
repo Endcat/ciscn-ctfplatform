05e8eda249536c05aa3a32baf2f6428bab48705b
Endcat <admin@endcat.cn>
2020-04-07 21:51:43 +0800

    Update loginBox

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <login-box base-width="500" base-height="600"></login-box>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  import loginBox from "./components/loginBox";
+  export default {
+    name: 'app',
+    components: {
+      loginBox,
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  html, body {
+    height: 100%;
+  }
+  #app {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    background-image: linear-gradient(to bottom right, rgb(201, 115, 255), rgb(174, 186, 248));
+  }
 </style>
---
+++ src/components/loginBox.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="base-container" :style="baseSize">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "baseObject",
+        props: {
+            baseWidth: String,
+            baseHeight: String,
+        },
+        data: function () {
+            return {
+                baseSize: {
+                    width: this.baseWidth + 'px',
+                    height: this.baseHeight + 'px',
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .base-container {
+        border-radius: 15px;
+        box-shadow: 0 10px 50px 0 rgb(107, 66, 217);
+    }
+</style>
